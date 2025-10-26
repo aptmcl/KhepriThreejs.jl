ca055bc7f078d4d4a179440c9e2eea7b261c2779
Documenter.jl <documenter@juliadocs.github.io>
2025-10-26 12:39:18 +0000

    build based on afbee0b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#KhepriThreejs","page":"Home","title":"KhepriThreejs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for KhepriThreejs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
